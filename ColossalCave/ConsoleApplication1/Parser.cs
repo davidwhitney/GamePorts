5f57e851d4852b2a53aad54d371cc7c04d3680f8
David Whitney <david@davidwhitney.co.uk>
2016-10-26 19:55:07 +0100

    Things.

---
+++ ColossalCave/ConsoleApplication1/Parser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    public class Parser
+    {
+        private readonly IFileSystem _fs;
+
+        public Parser(IFileSystem fs)
+        {
+            _fs = fs;
+        }
+
+        public Adventure Parse(string dataPath)
+        {
+            if (string.IsNullOrWhiteSpace(dataPath))
+            {
+                throw new ArgumentNullException(nameof(dataPath));
+            }
+
+            var lines = _fs.File.ReadAllLines(dataPath);
+            var adventure = new Adventure
+            {
+                RawLength = lines.Length
+            };
+
+            var chunks = SplitDataIntoChunks(lines);
+            var longForms = chunks.First();
+            foreach (var description in longForms)
+            {
+                var keyValue = description.Split('\t');
+
+                int locationId;
+                var locationIdAsString = keyValue[0];
+                var foundInteger = int.TryParse(locationIdAsString, out locationId);
+
+                if (!foundInteger) continue;
+
+                if (keyValue.Length == 1)
+                {
+                    adventure.LongFormDescriptions.Add(locationId, new LocationDescription {""});
+                }
+
+                var textString = keyValue[1];
+                adventure.LongFormDescriptions.Add(locationId, new LocationDescription {textString});
+            }
+
+            return adventure;
+        }
+
+        private static List<List<string>> SplitDataIntoChunks(IEnumerable<string> lines)
+        {
+            var chunks = new List<List<string>>();
+            var currentBlock = new List<string>();
+            foreach (var line in lines)
+            {
+                if (line == "-1")
+                {
+                    chunks.Add(currentBlock);
+                    currentBlock = new List<string>();
+                }
+                currentBlock.Add(line);
+            }
+
+            if (currentBlock.Any() 
+                && !chunks.Contains(currentBlock))
+            {
+                chunks.Add(currentBlock);
+            }
+
+            return chunks;
+        }
+    }
+
+    public class Adventure
+    {
+        public int RawLength { get; set; }
+        public Dictionary<int, LocationDescription> LongFormDescriptions { get; set; } = new Dictionary<int, LocationDescription>();
+        
+    }
+
+    public class LocationDescription : List<string>
+    {
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, this);
+        }
+    }
+}
---
+++ ColossalCave/Tests/ParserTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Text;
+using ConsoleApplication1;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ParserTests
+    {
+        private Parser _parser;
+        private MockFileSystem _fs;
+        private MockFileData _mockFileData;
+        private const string AdvenDat = @"C:\dev\GamePorts\ColossalCave\ConsoleApplication1\adven.dat";
+        private const string Rn = "\r\n";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fs = new MockFileSystem();
+            _parser = new Parser(_fs);
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Parse_PassedNullEmptyOrWhitespaceThrows(string dataPath)
+        {
+            Assert.Throws<ArgumentNullException>(() => _parser.Parse(dataPath));
+        }
+
+        [Test]
+        public void Parse_ProvidedFile_ParserKnowsHowBigTheFileIs()
+        {
+            FileDataIs("1 SOME TEXT");
+
+            var gameWorld = _parser.Parse(AdvenDat);
+
+            Assert.That(gameWorld.RawLength, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Parse_GivenLongFormDescriptions_Detects()
+        {
+            FileDataIs(@"1	YOU ARE STANDING AT THE END OF A ROAD BEFORE A SMALL BRICK BUILDING." + Rn +
+                       "-1" + Rn +
+                       "1	YOU'RE AT END OF ROAD AGAIN." + Rn +
+                       "-1" + Rn +
+                       "1	2	2	44	29");
+
+            var gameWorld = _parser.Parse(AdvenDat);
+
+            Assert.That(gameWorld.LongFormDescriptions.Count, Is.EqualTo(1));
+            Assert.That(gameWorld.LongFormDescriptions.First().Value.ToString(), Is.EqualTo("YOU ARE STANDING AT THE END OF A ROAD BEFORE A SMALL BRICK BUILDING."));
+        }
+
+        private void FileDataIs(string contents)
+        {
+            _mockFileData = new MockFileData(contents);
+            _fs.AddFile(AdvenDat, _mockFileData);
+        }
+    }
+}
